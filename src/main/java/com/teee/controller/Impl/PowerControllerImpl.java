523bbe2f3860d1bd67a7a29e92f66a1dc02cf950
许正韬 <1753059613@qq.com>
2022-12-14 00:17:36 +0800

    finish userinfo

--- src/main/java/com/teee/controller/Impl/PowerControllerImpl.java
+++ src/main/java/com/teee/controller/Impl/PowerControllerImpl.java
@@ -1,9 +1,16 @@
 package com.teee.controller.Impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.teee.config.Code;
+import com.teee.controller.PowerController;
+import com.teee.dao.UserFaceDao;
+import com.teee.dao.UserInfoDao;
+import com.teee.domain.UserFace;
 import com.teee.domain.UserInfo;
 import com.teee.domain.returnClass.Result;
 import com.teee.service.publicpart.Impl.PowerServiceImpl;
+import com.teee.utils.JWT;
 import com.teee.utils.SpringBeanUtil;
 import org.springframework.web.bind.annotation.*;
 
@@ -67,4 +74,34 @@ public class PowerControllerImpl implements PowerController {
 
         return res;
     }
+
+    @Override
+    @RequestMapping("/getInfo")
+    @ResponseBody
+    public Result getInfo(@RequestHeader("Authorization") String token){
+        try{
+            UserInfoDao bean = SpringBeanUtil.getBean(UserInfoDao.class);
+            UserFaceDao bean1 = SpringBeanUtil.getBean(UserFaceDao.class);
+            UserInfo userInfo = bean.selectById(JWT.getUid(token));
+            UserFace userFace = bean1.selectById(userInfo.getUid());
+            if(userInfo == null){
+                return new Result(Code.ERR,null,"获取用户信息失败!");
+            }else{
+                JSONObject o = (JSONObject) JSONObject.toJSON(userInfo);
+                if(userFace == null){
+                    o.put("face", "no");
+                }else if(userFace.getFaceSrc() == null || userFace.getFaceSrc().equals("")){
+                    o.put("face", "no");
+                }else{
+                    o.put("face", "yes");
+                }
+                return new Result(Code.Suc, o.toJSONString(), "获取成功");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(Code.ERR,e.getMessage(),e.getMessage());
+        }
+
+    }
+
 }
--- src/main/java/com/teee/controller/PowerController.java
+++ src/main/java/com/teee/controller/PowerController.java
@@ -1,9 +1,11 @@
-package com.teee.controller.Impl;
+package com.teee.controller;
 
 import com.teee.domain.returnClass.Result;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 public interface PowerController {
 
     Result getUser(String token);
     Result getRole(String token);
+    Result getInfo(String token);
 }
